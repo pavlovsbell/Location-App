9ca5599d0a7459a247cf09c6b0d7abcc68c4f739
Julianne <juliannebagsh@gmail.com>
2016-02-17 21:47:42 +0000

    Initial Commit

---
+++ Location AppTests/Location_AppTests.m
@@ -0,0 +1,40 @@
+//
+//  Location_AppTests.m
+//  Location AppTests
+//
+//  Created by Julianne on 17/02/2016.
+//  Copyright (c) 2016 Julianne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Location_AppTests : XCTestCase
+
+@end
+
+@implementation Location_AppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
